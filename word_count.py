7c89fc1d9d82ac67935c03003b80988208c065fa
Samir Agarwala <samirag@umich.edu>
2024-03-14 23:04:32 -0700

    added to word count analysis

--- word_count.py
+++ word_count.py
@@ -1,14 +1,24 @@
 import json
+import numpy as np
 
 with open('./data/final/final_data.json', 'r') as f:
     data = json.load(f)
 
 num_words = 0
 num_sentences = 0
+min_words = float('inf')
+max_words = 0
+word_list = []
 for sample in data:
     sentence = data[sample]["full sentence"]
     word_count_sentence = len(sentence.strip().split())
     num_words += word_count_sentence
     num_sentences += 1
+    min_words = min(min_words, word_count_sentence)
+    max_words = max(max_words, word_count_sentence)
+    word_list.append(word_count_sentence)
 
 print("Avg number of words per sentence: %d" % (num_words / num_sentences))
+print("Min number of words: %d" % (min_words))
+print("Max number of words: %d" % (max_words))
+print("Median number of words: %d" % (np.median(np.array(word_list))))
